ba8273658e5b48aa3039dfde31c9a29c3dc38aeb
ptrung <phantrung8298@gmail.com>
2025-10-08 16:11:12 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './modules/auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  
+}
